dcdb2843552173d8c3487084bb21593225aa806c
Alun 'Will' Williams <alun@alunw.com>
2021-06-27 16:20:32 +0100

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # README :: moany-api
 
 **WIP - All TODO**
+
+Using https://auth.alunw.com/auth as Keycloak server
+
+For moany-api: client_id = moany-api
+
+For front-end: client_id = moany-fe
+
--- src/main/java/com/alunw/moanyapi/controller/TestController.java
+++ src/main/java/com/alunw/moanyapi/controller/TestController.java
@@ -1,36 +1,48 @@
 package com.alunw.moanyapi.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "Authorization")
 public class TestController {
 
     @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
-    public ResponseEntity<String> getAnonymous() {
-        return ResponseEntity.ok("Hello Anonymous");
+    public ResponseEntity<Map<String, String>> getAnonymous() {
+        Map<String, String> result = new HashMap<>();
+        result.put("message", "Hello Anonymous");
+        return ResponseEntity.ok(result);
     }
 
     @RolesAllowed("user")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("Hello User");
+    public ResponseEntity<Map<String, String>> getUser() {
+        Map<String, String> result = new HashMap<>();
+        result.put("message", "Hello User");
+        return ResponseEntity.ok(result);
     }
 
     @RolesAllowed("admin")
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public ResponseEntity<String> getAdmin() {
-        return ResponseEntity.ok("Hello Admin");
+    public ResponseEntity<Map<String, String>> getAdmin() {
+        Map<String, String> result = new HashMap<>();
+        result.put("message", "Hello Admin");
+        return ResponseEntity.ok(result);
     }
 
-    @RolesAllowed({"admin","user"})
+//    @RolesAllowed({"admin","user"})
     @RequestMapping(value = "/all-user", method = RequestMethod.GET)
-    public ResponseEntity<String> getAllUser() {
-        return ResponseEntity.ok("Hello All User");
+    public ResponseEntity<Map<String, String>> getAllUser() {
+        Map<String, String> result = new HashMap<>();
+        result.put("message", "Hello All Users");
+        return ResponseEntity.ok(result);
     }
 }
