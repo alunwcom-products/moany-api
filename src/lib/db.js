96973ff1a78bc9af546d2ef787b0aabdf082944e
Alun 'Will' Williams <alun@alunw.com>
2025-01-27 16:13:53 +0000

    Completed authenticate function

--- src/lib/db.js
+++ src/lib/db.js
@@ -9,14 +9,24 @@ const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    // Cast bit(1) fields to boolean
+    typeCast: function (field, next) {
+        if (field.type === 'BIT' && field.length === 1) {
+            const bytes = field.buffer();
+            return ( bytes[ 0 ] === 1 );
+        } else {
+            return next();
+        }
+    }
 });
 
 async function authenticate(username, password, logger) {
     const [ results ] = await connection.query('select * from users where username = ?', [username]);
-    logger.info(JSON.stringify(results, null, 2));
+    logger.debug(JSON.stringify(results, null, 2));
     const success = 
         results.length > 0 &&
+        results[0].enabled &&
         results[0].username &&
         results[0].password &&
         results[0].username === username &&
