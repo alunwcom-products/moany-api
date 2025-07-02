a8795aeca7a3d48fe68b4b4e550b05794c808636
Alun 'Will' Williams <alun@alunw.com>
2025-07-02 11:36:41 +0100

    testing wip

---
+++ test/exp.test.js
@@ -0,0 +1,24 @@
+// import {
+//   expect, jest, test, beforeAll, describe
+// } from '@jest/globals';
+
+// describe('tests', () => {
+  // beforeAll(async () => {
+    // const fetchSpy = jest.spyOn(globalThis, 'fetch');
+    // fetchSpy.mockImplementation((input) => {
+    //   const found = okRequests.find((url) => url === input.url);
+    //   if (found) {
+    //     return new Response('Mocked success!', { status: 200 });
+    //   }
+    //   return new Response('Mocked not found!', { status: 404 });
+    // });
+  // });
+
+
+  test('simple test', async () => {
+    // console.log('Running simple test');
+    expect(true).toBe(true);
+  });
+
+// });
+
---
+++ test/exp2.test.mjs
@@ -0,0 +1,27 @@
+// import {
+//   expect, jest, test, beforeAll, describe
+// } from '@jest/globals';
+
+// import { generateJWT, verifyJWT, authenticate } from './utilities/auth.js';
+
+// describe('tests', () => {
+  beforeAll(async () => {
+    // const fetchSpy = jest.spyOn(globalThis, 'fetch');
+    // fetchSpy.mockImplementation((input) => {
+    //   const found = okRequests.find((url) => url === input.url);
+    //   if (found) {
+    //     return new Response('Mocked success!', { status: 200 });
+    //   }
+    //   return new Response('Mocked not found!', { status: 404 });
+    // });
+  });
+
+
+  test('simple test 2', async () => {
+    // console.log('Running simple test');
+    // const result = await authenticate('testuser', 'testpassword');
+    expect(true).toBe(true);
+  });
+
+// });
+
