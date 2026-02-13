0b20ca8ccec93ef38a386dec92ad5dcc2dcbba16
Alun 'Will' Williams <alun@alunw.com>
2026-02-13 11:18:12 +0000

    remove filesystem pdf handling

--- src/lib/pdf.js
+++ src/lib/pdf.js
@@ -2,20 +2,18 @@
 // PDF utilities, inc. file access
 //
 
-import fs from 'fs';
 import pdfParse from 'pdf-parse-new';
 import logger from './logger.js';
 
-async function extractPdfText(pdfPathOrBuffer) {
-    const dataBuffer = typeof pdfPathOrBuffer === 'string' ? fs.readFileSync(pdfPathOrBuffer) : pdfPathOrBuffer;
-    try {
-        const data = await pdfParse(dataBuffer, { verbosityLevel: 0 }); // output errors only
-        // logger.debug('Extracted text from PDF: %s', data.text);
-        return data.text;
-    } catch (error) {
-        logger.error('Error parsing PDF:', error);
-        return null;
-    }
+async function extractPdfText(pdfBuffer) {
+  try {
+    const data = await pdfParse(pdfBuffer, { verbosityLevel: 0 }); // output errors only
+    // logger.debug('Extracted text from PDF: %s', data.text);
+    return data.text;
+  } catch (error) {
+    logger.error('Error parsing PDF:', error);
+    return null;
+  }
 }
 
 export {
