a0a1940311d2a5120ff544c8470e314dc9c5ffa7
Alun 'Will' Williams <alun@alunw.com>
2026-02-16 13:40:36 +0000

    pondering decimal numbers...
    ...stick with strings for now

--- src/lib/db.js
+++ src/lib/db.js
@@ -10,6 +10,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  // decimalNumbers: true,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
