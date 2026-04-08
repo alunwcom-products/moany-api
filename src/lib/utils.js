1903583dcb1ca2cd8a3856640e757e47029ccf24
Alun 'Will' Williams <alun@alunw.com>
2026-04-08 19:31:57 +0100

    switch key to uuidv7

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,11 +1,9 @@
 import { uuidv7 } from "uuidv7";
-import { v4 as uuidv4 } from 'uuid';
 
 // abstract key creation
 // currently using uuidv4 but planning to migrate to uuidv7
 function getKey() {
-  // return uuidv7();
-  return uuidv4();
+  return uuidv7();
 }
 
 export {
