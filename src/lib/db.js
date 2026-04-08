3734a91ae649f65b5baf6e89df15f7bdbc3fa50e
Alun 'Will' Williams <alun@alunw.com>
2026-04-08 13:44:20 +0100

    Fix db port config

--- src/lib/db.js
+++ src/lib/db.js
@@ -10,6 +10,7 @@ import { formatDate } from './date.js';
 // mysql2 connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
