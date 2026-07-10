4e88f16a51944f2654a31044cbe70fb4273f6a31
Alun 'Will' Williams <alun@alunw.com>
2026-07-10 16:19:34 +0100

    update value for 'Uncategorized' category

--- src/lib/db.js
+++ src/lib/db.js
@@ -154,14 +154,15 @@ const getTransactions = async (limit, offset, accounts, categories, includeChild
   // handle categories + include child categories
   if (categories && categories.length > 0) {
 
-    // check for 'uncat' (Uncategorized) special category
-    const uncat = categories.filter((uuid) => uuid === 'uncat');
+    // check for null (Uncategorized) category
+    const uncat = categories.filter((uuid) => uuid === 'null');
     if (uncat.length === 0) {
       whereSql += ' AND category IN ';
     } else {
       whereSql += ' AND category IS NULL OR category IN ';
     }
 
+    // check for 'include child categories'
     if (includeChildCategories === 'true' || includeChildCategories === true) {
       // look up the category itself AND all its descendants
       whereSql += '(SELECT descendant_id FROM v_category_hierarchy WHERE parent_id IN (?))';
