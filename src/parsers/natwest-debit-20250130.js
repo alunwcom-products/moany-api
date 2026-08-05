bd2110a832530e4c370ccdc5310de8a22658bebc
Alun 'Will' Williams <alun@alunw.com>
2026-08-05 17:03:59 +0100

    Updated natwest debit parser...
    ..to handle 'random' text at end of statements.

--- src/parsers/natwest-debit-20250130.js
+++ src/parsers/natwest-debit-20250130.js
@@ -28,45 +28,102 @@ import { getKey } from "../lib/utils.js";
 import logger from "../lib/logger.js";
 import { getAccountByNumber } from "../lib/db.js";
 
+const TX_AMOUNT_REGEX = / ?\d{1,3}(?:,\d{3})*\.\d{2}$/; // used to check for the end of a transaction line
+
 export default async function parseStatement(pdfText, pdfFilename) {
+  logger.debug('start parseTransactions()');
+  const transactions = parseTransactions(pdfText, pdfFilename);
+  logger.debug('completed parseTransactions()');
+  return transactions;
+}
 
-  logger.debug('parseTransactions() called');
 
-  const accountNumber = getAccountNumber(pdfText)[1].replaceAll(' ', '');
-  const account = await getAccountByNumber(accountNumber);
+async function parseTransactions(pdfText, pdfFilename) {
 
-  let sourceRow = 0;
-  let previousDate;
-  let previousBalance;
+  // Before actually parsing the transactions, the statement next needs to be processed
+  // to remove much of the non-transaction text which would prevent transactions being
+  // parsed corrected.
+  // This is done by removing the 'pre-transaction' text, then the 'post-transaction'
+  // text, and lastly any text that appears as headers and footers between pages.
+  // The text is initially split into an array of lines to assist processing, and allow
+  // multiline transactions to subsequently be merged together.
 
-  // initialize transactions - adding extra transaction for starting balance
-  const transactions = [];
-  let match;
-  // format: '26 OCT 28 OCT 00498929 FASTER PAYMENT RECEIVED - THANK YOU 1,481.41 -' 
-  const transactionRegex = /^(\d{2} [A-Z]{3}(?: \d{4})?)?(.*?)(\d{1,3}(?:,\d{3})*\.\d{2} )?(\d{1,3}(?:,\d{3})*\.\d{2})$/gms;
+  // Split text into array of lines (removing leading/trailing whitespace, empty lines)
+  const rawLines = pdfText.split(/\r?\n/).map(l => l.trim()).filter(Boolean);
 
-  while ((match = transactionRegex.exec(getTransactionText(pdfText))) !== null) {
-
-    //logger.debug(JSON.stringify(match, null, 2));
-
-    let transDate = getTransactionDate(match[1], previousDate);
-    if (!previousDate && !transDate) {
-      throw new Error('No transaction date or previous date found');
+  // Remove text before first transaction (i.e. before first 'BROUGHT FORWARD' line)
+  let skipLines = true;
+  const editedText1 = rawLines.filter(line => {
+    if (!skipLines) return true;
+    if (line.toUpperCase().includes('BROUGHT FORWARD')) {
+      skipLines = false;
+      return true;
     }
-    if (!transDate) {
-      transDate = previousDate;
-    } else {
-      previousDate = transDate;
+    return false;
+  });
+
+  // Remove text after last transaction balance (i.e. currency amount at end of line) NOTE: working from end to beginning
+  skipLines = true;
+  const editedText2 = editedText1.toReversed().filter(line => {
+    if (!skipLines) return true;
+    if (TX_AMOUNT_REGEX.test(line)) { // relies on only transaction lines ending currency format without £
+      skipLines = false;
+      return true;
     }
+    return false;
+  });
 
-    const statement_balance = getAmountFromString(match[4]);
+  // Remove text between pages (i.e. lines from 'RETSTMT' to 'Balance(£)') NOTE: re-reverse the order
+  skipLines = false;
+  const editedText3 = editedText2.toReversed().filter(line => {
+    // Check if transactions ended for page
+    if (line.startsWith('RETSTMT')) {
+      skipLines = true;
+      return false;
+    }
+    if (skipLines && line.endsWith('Balance(£)')) {
+      skipLines = false;
+      return false;
+    }
+    if (skipLines) return false;
+    // else
+    return true;
+  });
 
+  // merge multi-line transactions
+  const txLines = mergeTransactions(editedText3);
+
+  // logger.debug(JSON.stringify(txLines, null, 2));
+
+  // get account so account uuid can be added to transactions
+  const account = await getAccountByNumber(getAccountNumber(pdfText));
+
+  // parse transaction details and add to output array to be returned
+  const txRegex = /^(?<date>\d{2}\s+[A-Z]{3}(?:\s+\d{4})?)?(?:\s*(?<description>.*?))(?:\s+?(?<amount>-?(?:0|[1-9]\d{0,2}(?:,\d{3})*|\d+)\.\d{2}))?\s+?(?<balance>-?(?:0|[1-9]\d{0,2}(?:,\d{3})*|\d+)\.\d{2})$/;
+  
+  const txs = [];
+  let lastDate; // store last date for transactions without date
+  let previousBalance; // track balance to see if transaction amount is positive or negative (credit or debit)
+  let sourceRow = 0; // counter for sequence of transactions on statement
+
+  txLines.forEach(tx => {
+
+    const match = txRegex.exec(tx);
+    const { date, description, amount, balance } = match.groups;
+
+    // get transaction date
+    lastDate = getTransactionDate(date, lastDate);
+ 
+    // get balance value
+    const statement_balance = getAmountFromString(balance);
+
+    // calculate whether amount is debit or credit (all show as positive values on statement)
     let statement_amount;
-    if (match[3]) {
-      statement_amount = getAmountFromString(match[3]);
+    if (amount) {
+      statement_amount = getAmountFromString(amount);
       // need to check if this is paid in or withdrawn
       if (!previousBalance) {
-        // first transaction is b/f balance - acn ignore this
+        // first transaction is b/f balance - can ignore this
       } else if (previousBalance > statement_balance) {
         statement_amount = statement_amount * -1;
       }
@@ -74,16 +131,14 @@ export default async function parseStatement(pdfText, pdfFilename) {
       statement_amount = 0;
     }
 
+    // update previous balance
     previousBalance = statement_balance;
 
+    // net amount will always equal statement amount (this has not always been the case when statements were imported as CSV data)
     const net_amount = statement_amount;
 
-    let description = match[2];
-    if (description) {
-      description = description.replace('\n', ' ').replace(/\s\s+/g, ' ').trim();
-    }
-
-    transactions.push({
+    // add transaction to output array
+    txs.push({
       uuid: getKey(),
       statement_amount,
       description,
@@ -94,22 +149,54 @@ export default async function parseStatement(pdfText, pdfFilename) {
       source_type: 'STATEMENT',
       statement_balance,
       account_balance: null,
-      trans_date: transDate,
+      trans_date: lastDate,
       type: null,
       account: account.uuid,
       category: null,
       net_amount
     });
-  }
+  });
 
   // sort on transaction date and source row (mastercard payment is not shown in order on statement)
-  transactions.sort((a, b) => a.trans_date - b.trans_date || a.sourceRow - b.sourceRow);
+  txs.sort((a, b) => a.trans_date - b.trans_date || a.sourceRow - b.sourceRow);  
 
-  logger.debug('parseTransactions() completed successfully');
+  // logger.debug(JSON.stringify(txs, null, 2));
 
-  return transactions;
+  return txs;
 }
 
+// Merge multiline transactions
+function mergeTransactions(txLines) {
+  const mergedTxLines = []; // output array
+  let multilineStr = null;
+
+  txLines.forEach(txLine => {
+    if (multilineStr != null) { // multi-line transaction started
+      if (/^\d{2} [A-Z]{3} /.test(txLine)) { // starts with date - invalid state
+        logger.error(`Unexpected multi-line tx state: '${txLine}'`);
+      } else {
+        if (TX_AMOUNT_REGEX.test(txLine)) { // ends with amount - must be end of multi-line tx
+          multilineStr += ` ${txLine}`;
+          mergedTxLines.push(multilineStr);
+          multilineStr = null;
+        } else { // no amount - must be middle of multi-line tx
+          logger.debug(`mid tx: ${txLine}`);
+          multilineStr += ` ${txLine}`;
+        }
+      }
+    } else {
+      if (TX_AMOUNT_REGEX.test(txLine)) { // ends with amount - must be single line transaction (with or without date at start of line)
+        mergedTxLines.push(txLine);
+      } else {
+        multilineStr = txLine;
+      }
+    }
+  });
+
+  return mergedTxLines;
+}
+
+// TODO parse amounts - use currency type?
 function getAmountFromString(amountStr, signStr = '') {
   const sign = (signStr.trim() === '-' ? '-' : '+');
   return parseFloat(sign + amountStr.trim().replaceAll(',', ''));
@@ -118,36 +205,22 @@ function getAmountFromString(amountStr, signStr = '') {
 // get account number from statement
 // format: '87275643 60-01-04'
 function getAccountNumber(text) {
-  const regex = /(\d{8})\s(\d{2}-\d{2}-\d{2})/g;
+  const regex = /(?<accountNumber>\d{8})\s(?<sortCode>\d{2}-\d{2}-\d{2})/;
   const match = regex.exec(text);
-  if (!match || match.length !== 3) {
+  const { accountNumber, sortCode } = match.groups;
+  if (!accountNumber) {
     throw new Error('Account number not found in text');
   }
-  return match;
-}
-
-// remove text that sits between statement pages, and
-// get the statement text from the start of the transactions
-// to remove summary amounts and text
-function getTransactionText(text) {
-  // remove text that sits between statement pages
-  const editedText = text.replace(/RETSTMT .*?Balance\(\£\)/gs, "");
-
-  //logger.debug(editedText);
-
-  // match text from the first 'Balance(£)' heder
-  const regex = /Balance\(£\)\n?(.*)/gms;
-  const match = regex.exec(editedText);
-  if (!match || match.length !== 2) {
-    throw new Error('Transaction text not found');
-  }
-  return match[1];
+  return accountNumber.replaceAll(' ', '');
 }
 
 // parse the transaction date - may be null, or 2 formats
 function getTransactionDate(dateStr, refDate) {
   if (!dateStr || dateStr.trim().length === 0) {
-    return null;
+    if (!refDate) {
+      throw new Error('No transaction date or previous date found');
+    }
+    return refDate;
   }
   const longDate = parse(dateStr.trim(), 'dd MMM yyyy', null); // must include the year
   if (isValid(longDate)) {
@@ -158,5 +231,5 @@ function getTransactionDate(dateStr, refDate) {
     return shortDate;
   }
 
-  return null;
+  throw new Error(`Error parsing date: '${dateStr}'`);
 }
